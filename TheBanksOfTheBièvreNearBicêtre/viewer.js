13c310f3448e78cf24b03cd88c80318b6c26e8fa
Nick Fitzsimons <nick@nickfitz.co.uk>
2019-09-03 20:40:50 +0100

    Adjust view configuration.

--- TheBanksOfTheBièvreNearBicêtre/viewer.js
+++ TheBanksOfTheBièvreNearBicêtre/viewer.js
@@ -1,20 +1,15 @@
 const map = (() => {
-
   const pictureLayer = new ol.layer.Tile({
-    // extent: ol.proj.transformExtent([3110, -3710.0, 3110.0, 3710.0], 'EPSG:4326', 'EPSG:3857'),
     source: new ol.source.TileJSON({
-      url: './tiles.json',
+      url: 'https://earthmarks.github.io/TiledArt-Rousseau/TheBanksOfTheBièvreNearBicêtre/tiles.json',
     })
   });
 
   const view = new ol.View({
     center: [1855,1555],
-    zoom: 2,
-    // maxResolution: 16.000000,
-    // maxZoom: 4,
-    // minZoom: 0,
-    // projection: "EPSG:3857",
-    // displayProjection: new ol.proj.Projection("EPSG:4326")
+    zoom: 0,
+    maxZoom: 4,
+    minZoom: 0,
   });
 
   const map = new ol.Map({
